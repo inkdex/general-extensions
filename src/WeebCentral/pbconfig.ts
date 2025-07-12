7ce10d6209d5d540f33f89ab50efe9794c1b578d
GabrielCWT <77312579+GabrielCWT@users.noreply.github.com>
2025-07-12 23:28:49 +0800

    chore(WeebCentral): Update developer info

--- src/WeebCentral/pbconfig.ts
+++ src/WeebCentral/pbconfig.ts
@@ -15,9 +15,8 @@ export default {
     badges: [],
     developers: [
         {
-            name: "Inkdex",
-            website: "https://inkdex.github.io",
-            github: "https://github.com/inkdex",
+            name: "GabrielCWT",
+            github: "https://github.com/GabrielCWT",
         },
     ],
 } satisfies SourceInfo;
