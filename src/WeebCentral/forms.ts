b2587400679c6ec94ff21329472306cd55d48af3
GabrielCWT <77312579+GabrielCWT@users.noreply.github.com>
2025-07-17 21:50:50 +0800

    chore(WeebCentral): Remove redundant button

--- src/WeebCentral/forms.ts
+++ src/WeebCentral/forms.ts
@@ -1,15 +1,9 @@
-import {
-    ButtonRow,
-    Form,
-    FormSectionElement,
-    LabelRow,
-    Section,
-} from "@paperback/types";
+import { ButtonRow, Form, FormSectionElement, Section } from "@paperback/types";
 
 export class SettingsForm extends Form {
     override getSections(): FormSectionElement[] {
         return [
-            Section("second", [
+            Section("tags", [
                 ButtonRow("clearTags", {
                     title: "Clear Cached Search Tags",
                     onSelect: Application.Selector(
@@ -17,27 +11,11 @@ export class SettingsForm extends Form {
                         "clearTags",
                     ),
                 }),
-                ButtonRow("resetState", {
-                    title: "Reset All State",
-                    onSelect: Application.Selector(
-                        this as SettingsForm,
-                        "resetState",
-                    ),
-                }),
-                LabelRow("resetStateLabel", {
-                    title: "",
-                    subtitle:
-                        "Clicking this will reset all state for this extension. Do not click unless you know what you are doing.",
-                }),
             ]),
         ];
     }
 
     async clearTags(): Promise<void> {
         Application.setState(undefined, "tags");
-    }
-
-    async resetState(): Promise<void> {
-        Application.resetAllState();
     }
 }
