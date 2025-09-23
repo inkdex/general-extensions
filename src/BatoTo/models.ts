ce2e8e03ad43c8f988b9bb676da2f66dc0eba22d
Nicholas Lim <niclimcongyi@gmail.com>
2025-09-23 10:14:04 +0800

    chore(BatoTo): Clean-up unused types

---
+++ src/BatoTo/models.ts
@@ -0,0 +1,1 @@
+export type Metadata = { offset?: number; collectedIds?: string[] };
--- src/BatoTo/pbconfig.ts
+++ src/BatoTo/pbconfig.ts
@@ -3,7 +3,7 @@ import { ContentRating, SourceInfo, SourceIntents } from "@paperback/types";
 export default {
     name: "Bato.To",
     description: "Extension that pulls content from bato.to.",
-    version: "1.0.0-alpha.1",
+    version: "1.0.0-alpha.2",
     icon: "icon.png",
     language: "en",
     contentRating: ContentRating.ADULT,
