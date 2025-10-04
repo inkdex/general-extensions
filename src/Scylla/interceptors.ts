faae15b7972533c47c2d096c7225554eb01632ec
LucifersCircle <31424518+LucifersCircle@users.noreply.github.com>
2025-10-04 10:24:56 -0700

    Update interceptors.ts
    refactor(Scylla): remove user agent from request interceptor

--- src/Scylla/interceptors.ts
+++ src/Scylla/interceptors.ts
@@ -1,15 +1,16 @@
-import { PaperbackInterceptor, Request, Response } from "@paperback/types";
-import { SCYLLA_DOMAIN } from "./main";
+import { 
+    PaperbackInterceptor,
+    Request,
+    Response,
+} from "@paperback/types";
+import { 
+    SCYLLA_DOMAIN,
+} from "./main";
 
 export class ScyllaInterceptor extends PaperbackInterceptor {
     async interceptRequest(request: Request): Promise<Request> {
-        request.headers = {
-            ...(request.headers ?? {}),
-            ...{
-                referer: `${SCYLLA_DOMAIN}/`,
-                "user-agent": await Application.getDefaultUserAgent(),
-            },
-        };
+        request.headers = request.headers ?? {};
+        request.headers.referer = `${SCYLLA_DOMAIN}/`;
         return request;
     }
 
