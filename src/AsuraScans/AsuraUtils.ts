e6c54be406069a0845d54da1a0e16384ded72dac
Saw_6 <virgile@pc-254.home>
2025-11-22 12:53:09 +0100

    fix(Asura): fix manga ID parsing to include suffix

--- src/AsuraScans/AsuraUtils.ts
+++ src/AsuraScans/AsuraUtils.ts
@@ -13,7 +13,7 @@ export async function getFilter(filter: string): Promise<string> {
 }
 
 export async function getMangaId(slug: string): Promise<string> {
-    const id = idCleaner(slug) + "-";
+    const id = idCleaner(slug);
 
     return id;
 }
@@ -22,8 +22,6 @@ function idCleaner(str: string): string {
     let cleanId: string | null = str;
     cleanId = cleanId.replace(/\/$/, "");
     cleanId = cleanId.split("/").pop() ?? null;
-    // Remove randomised slug part
-    cleanId = cleanId?.substring(0, cleanId?.lastIndexOf("-")) ?? null;
 
     if (!cleanId) {
         throw new Error(`Unable to parse id for ${str}`);
