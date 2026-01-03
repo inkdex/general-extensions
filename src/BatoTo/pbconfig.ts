2277d163afa9b0018a0dd8392f7e0dcbee64886c
Nicholas Lim <niclimcongyi@gmail.com>
2026-01-04 01:16:36 +0800

    fix(BatoTo): Switch to working mirror

--- src/BatoTo/pbconfig.ts
+++ src/BatoTo/pbconfig.ts
@@ -1,17 +1,21 @@
-import { ContentRating, SourceIntents, type SourceInfo } from "@paperback/types";
+import {
+  ContentRating,
+  SourceIntents,
+  type ExtensionInfo,
+} from "@paperback/types";
 
 export default {
   name: "Bato.To",
   description: "Extension that pulls content from bato.to.",
-  version: "1.0.0-alpha.4",
+  version: "1.0.0-alpha.5",
   icon: "icon.png",
   language: "en",
   contentRating: ContentRating.ADULT,
   capabilities: [
-    SourceIntents.SETTINGS_UI,
-    SourceIntents.DISCOVER_SECIONS,
-    SourceIntents.MANGA_SEARCH,
-    SourceIntents.MANGA_CHAPTERS,
+    SourceIntents.SETTINGS_FORM_PROVIDING,
+    SourceIntents.DISCOVER_SECIONS_PROVIDING,
+    SourceIntents.SEARCH_RESULTS_PROVIDING,
+    SourceIntents.CHAPTER_PROVIDING,
   ],
   badges: [],
   developers: [
@@ -21,4 +25,4 @@ export default {
       github: "https://github.com/inkdex",
     },
   ],
-} satisfies SourceInfo;
+} satisfies ExtensionInfo;
