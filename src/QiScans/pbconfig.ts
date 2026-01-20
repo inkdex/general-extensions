9b9bce7260c931feff85b0c4afd4eba2599810dd
LucifersCircle <31424518+LucifersCircle@users.noreply.github.com>
2026-01-20 12:28:15 -0800

    fix(QiScans): add sorting by numbers in image filename

--- src/QiScans/pbconfig.ts
+++ src/QiScans/pbconfig.ts
@@ -4,7 +4,7 @@ import { ContentRating, SourceIntents } from "@paperback/types";
 export default {
   name: "QiScans",
   description: "Extension that pulls content from qiscans.org.",
-  version: "1.0.0-alpha.1",
+  version: "1.0.0-alpha.2",
   icon: "icon.png",
   language: "en",
   contentRating: ContentRating.EVERYONE,
