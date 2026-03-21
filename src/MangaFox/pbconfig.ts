06095362ed7501635ae1f4eee07d7ea94965495e
Nicholas Lim <6513413+niclimcy@users.noreply.github.com>
2026-03-21 15:21:50 +0800

    feat(MangaFox): Use toolchain URL class instead

--- src/MangaFox/pbconfig.ts
+++ src/MangaFox/pbconfig.ts
@@ -3,7 +3,7 @@ import { ContentRating, SourceIntents, type ExtensionInfo } from "@paperback/typ
 export default {
   name: "MangaFox",
   description: "Extension that pulls content from fanfox.net.",
-  version: "1.0.0-alpha.9",
+  version: "1.0.0-alpha.10",
   icon: "icon.png",
   language: "en",
   contentRating: ContentRating.MATURE,
