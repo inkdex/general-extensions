d8829116ea375e764f89f6be82343eb5354fafd9
Saw_6 <57862464+Thinkaz@users.noreply.github.com>
2026-04-23 12:37:47 +0200

    feat: adding roliascan

---
+++ src/Roliascan/network.ts
@@ -0,0 +1,38 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+/* Copyright © 2026 Inkdex */
+
+import { CloudflareError, PaperbackInterceptor, type Request, type Response } from "@paperback/types";
+import { DOMAIN } from "./models";
+
+export class RoliascanInterceptor extends PaperbackInterceptor {
+  async interceptRequest(request: Request): Promise<Request> {
+    return {
+      ...request,
+      headers: {
+        ...request.headers,
+        referer: `${DOMAIN}/`,
+        origin: DOMAIN,
+        "user-agent": await Application.getDefaultUserAgent(),
+      },
+    };
+  }
+
+  override async interceptResponse(
+    request: Request,
+    response: Response,
+    data: ArrayBuffer,
+  ): Promise<ArrayBuffer> {
+    const cfMitigated = response.headers?.["cf-mitigated"];
+    if (cfMitigated === "challenge") {
+      throw new CloudflareError({
+        url: `${DOMAIN}/`,
+        method: request.method ?? "GET",
+        headers: {
+          "user-agent": await Application.getDefaultUserAgent(),
+        },
+      });
+    }
+
+    return data;
+  }
+}
