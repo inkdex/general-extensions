220d0428ede0afb8287b49c77058fd21f6bd1ca8
Nicholas Lim <6513413+niclimcy@users.noreply.github.com>
2026-04-24 16:56:18 +0800

    fix(Comix): Toolchain removed Buffer polyfill

--- src/ComixTo/pbconfig.ts
+++ src/ComixTo/pbconfig.ts
@@ -6,7 +6,7 @@ import { ContentRating, SourceIntents, type ExtensionInfo } from "@paperback/typ
 export default {
   name: "ComixTo",
   description: "Shim of the Comix extension which pulls content from Comix.to.",
-  version: "1.0.0-alpha.2",
+  version: "1.0.0-alpha.3",
   icon: "icon.png",
   language: "en",
   contentRating: ContentRating.EVERYONE,
