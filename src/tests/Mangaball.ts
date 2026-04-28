235f438fdf0aeb7a1df58a461d28b974d9be4acd
karrot0 <karrotgaming1@gmail.com>
2026-04-27 22:41:01 -0400

    fix: update Mangaball test suite

--- src/tests/Mangaball.ts
+++ src/tests/Mangaball.ts
@@ -1,9 +1,11 @@
-import { TestSuite, registerDefaultTests } from "./suite.js";
-import { Mangaball } from "../Mangaball/main.js";
-import sourceInfo from "../MangaDex/pbconfig.js";
+import { type TestLogger } from "@paperback/types";
 
-export async function runTests() {
-  const suite = new TestSuite("Mangaball tests");
+import { Mangaball } from "../Mangaball/main.js";
+import sourceInfo from "../Mangaball/pbconfig.js";
+import { TestSuite, registerDefaultTests } from "./suite.js";
+
+export async function runTests(logger: TestLogger) {
+  const suite = new TestSuite("Mangaball tests", logger);
   registerDefaultTests(suite, Mangaball, sourceInfo);
 
   await suite.run();
