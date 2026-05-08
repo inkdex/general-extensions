ad5e2902eb4b37903bdf61fdbeaafdb7986022c5
LucifersCircle <31424518+LucifersCircle@users.noreply.github.com>
2026-05-08 05:52:39 -0700

    chore(lucifers-suite): version bumps

--- src/QiScans/pbconfig.ts
+++ src/QiScans/pbconfig.ts
@@ -7,7 +7,7 @@ import { ContentRating, SourceIntents } from "@paperback/types";
 export default {
   name: "QiScans",
   description: "Extension that pulls content from qimanhwa.com.",
-  version: "1.0.0-alpha.13",
+  version: "1.0.0-alpha.14",
   icon: "icon.png",
   language: "en",
   contentRating: ContentRating.EVERYONE,
