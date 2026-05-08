bd2869570fd1d927db514755d72bf2f14de14647
LucifersCircle <31424518+LucifersCircle@users.noreply.github.com>
2026-05-08 05:52:39 -0700

    chore(lucifers-suite): version bumps

--- src/Atsumaru/pbconfig.ts
+++ src/Atsumaru/pbconfig.ts
@@ -7,7 +7,7 @@ import { ContentRating, SourceIntents } from "@paperback/types";
 export default {
   name: "Atsumaru",
   description: "Extension that pulls content from atsu.moe.",
-  version: "1.0.0-alpha.18",
+  version: "1.0.0-alpha.19",
   icon: "icon.png",
   language: "en",
   contentRating: ContentRating.EVERYONE,
