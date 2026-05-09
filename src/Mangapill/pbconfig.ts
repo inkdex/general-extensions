6dd6bdba7f599adf2d8ea808ee151161dae46323
GabrielCWT <77312579+GabrielCWT@users.noreply.github.com>
2026-05-09 08:00:39 +0800

    chore(Mangapill): Update versions

--- src/Mangapill/pbconfig.ts
+++ src/Mangapill/pbconfig.ts
@@ -6,7 +6,7 @@ import { ContentRating, SourceIntents, type ExtensionInfo } from "@paperback/typ
 export default {
   name: "Mangapill",
   description: "Extension that pulls content from mangapill.com.",
-  version: "1.0.0-alpha.9",
+  version: "1.0.0-alpha.10",
   icon: "icon.png",
   language: "en",
   contentRating: ContentRating.EVERYONE,
