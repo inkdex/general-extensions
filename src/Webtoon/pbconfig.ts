af5df8ce2509ebdf84fdc68a64bd5a06f52e9a2a
rbertus2000 <91765399+rbertus2000@users.noreply.github.com>
2026-05-14 19:58:19 +0200

    fix(Webtoon): fix authors, missing space in long titles and square cover

--- src/Webtoon/pbconfig.ts
+++ src/Webtoon/pbconfig.ts
@@ -6,7 +6,7 @@ import { ContentRating, SourceIntents, type ExtensionInfo } from "@paperback/typ
 export default {
   name: "Webtoon",
   description: `Extension that pulls content from webtoons.com.`,
-  version: "1.0.0-alpha.15",
+  version: "1.0.0-alpha.16",
   icon: "icon.png",
   languages: "multi",
   contentRating: ContentRating.EVERYONE,
